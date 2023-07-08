b0932752694fde3059eacb14cfb56cfa5d6f6f2c
Mir Sajidur Rahman <msr.sajid1283@gmail.com>
2023-07-08 15:39:01 +0600

    WordsForTesting

---
+++ Words.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+
+words = [
+  ["car", "automobile", "vehicle", "acr", "cr"],
+  ["dog", "canine", "pooch", "dgo", "dogg"],
+  ["book", "novel", "publication", "bok", "boook"],
+  ["computer", "pc", "laptop", "comuter", "latop"],
+  ["city", "town", "metropolis", "ctiy", "citi"],
+  ["pen", "marker", "writing instrument", "pn", "pek"],
+  ["tree", "oak", "pine", "trew", "tere"],
+  ["cat", "feline", "kitty", "ct", "kat"],
+  ["bicycle", "bike", "cycle", "bicycel", "byke"],
+  ["house", "home", "dwelling", "huse", "hme"],
+  ["phone", "mobile", "cellular", "pone", "phne"],
+  ["shirt", "blouse", "top", "shit", "shrt"],
+  ["chair", "seat", "furniture", "cair", "cheir"],
+  ["shoe", "footwear", "sneaker", "soe", "shu"],
+  ["plant", "flora", "vegetation", "plnt", "pant"],
+  ["cup", "mug", "beverage container", "cp", "cupp"],
+  ["watch", "timepiece", "wristwatch", "wath", "wtch"]
+]
+
+words = pd.DataFrame(words, columns=["Word", "Synonym1", "Synonym2", "Typo1", "Typo2"])
+
+typos = words[["Word","Typo1", "Typo2"]]
