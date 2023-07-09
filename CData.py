da174cbe39bc3ac2dd2c127c89e6f1d34533d9af
Mir Sajidur Rahman <msr.sajid1283@gmail.com>
2023-07-09 23:11:25 +0600

    Added Fuzzymatching

--- CData.py
+++ CData.py
@@ -1,14 +1,48 @@
 import pandas as pd
 import re
 from spellchecker import SpellChecker
+from thefuzz import process
 
 
-#Fixing typos
+class base(pd.DataFrame):
+    
+    def new_cols(self,skip_first=None): #Issue: Adds cols after all cols, not just the ones with mistakes
+        
+        null_cols = []
+        for col in self.columns[1:] if skip_first else self.columns:
+            null_cols.append(col + "_new")
+            col_pos = self.columns.get_loc(col)
+            self.insert(col_pos + 1, null_cols[-1], None)
+                    
+        return null_cols
 
 
-class DF(pd.DataFrame):
 
-    def reduce_repeats(self):
+    def application(self,func,skip_first=None):
+        
+        null_cols = self.new_cols(skip_first)
+        iter_nulls = iter(null_cols)
+            
+        for col in self.columns[1:] if skip_first else self.columns:
+            if col not in null_cols:
+                corrected = []
+                
+                for word in self[col]:
+                    corrected.append(func(word))
+                add_col = next(iter_nulls)
+                self[add_col] = self[add_col].fillna(pd.Series(corrected))
+            
+        return self
+            
+
+class CD(base):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+      
+        
+      
+    def reduce_repeats(self): #Replaces words in place
 
         pattern = re.compile(r"(.)\1{2,}")
 
@@ -16,39 +50,34 @@ class DF(pd.DataFrame):
             return pattern.sub(r"\1\1", str(word))
 
         return self.applymap(apply_reduction)
-    
-    
+
+
 
     def spell_check(self):
-        
         spell = SpellChecker()
+    
+        def apply_correction(word):
+            return spell.correction(word)
+    
+        return self.application(apply_correction,skip_first=False)
+    
+    
+    
+    def fuzzed(self):
         
-        for col in self.columns:
+        def apply_correction(word):
+            return process.extractOne(word, self.iloc[:,0])[0]
             
-            new_col = col+"_new"
-            col_pos = self.columns.get_loc(col)
-            corrected = []
-            
-            for misspelled in self[col]:
-                corrected.append(spell.correction(str(misspelled))) 
-            self.insert(col_pos +1, new_col, corrected)
-            corrected = []
+        return self.application(apply_correction,skip_first=True)
         
-        return self
-        
-            
 
-        
+countries = pd.read_csv("countries.csv")
 typos = pd.read_csv("typos.csv")
-typos.columns = typos.columns.str.lower()
 
 
-removed = DF(typos).reduce_repeats()
+removed = CD(typos).reduce_repeats()
 
-checked = DF(removed).spell_check()
-
-
-
-
-
+typos_checked = CD(typos).spell_check()
+countries_checked = CD(countries).spell_check()
+fuzzings = CD(countries).fuzzed()
 
