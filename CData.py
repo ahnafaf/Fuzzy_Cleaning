0ef3c168408b880e95a36e225edc96509b9f26a4
Mir Sajidur Rahman <msr.sajid1283@gmail.com>
2023-07-11 14:47:55 +0600

    Added Meta Labelling

--- CData.py
+++ CData.py
@@ -2,15 +2,19 @@ import pandas as pd
 import re
 from spellchecker import SpellChecker
 from thefuzz import process
+import os
+import openai
+import numpy as np
+from sklearn.cluster import KMeans
 
 
-class base(pd.DataFrame):
+class CD(pd.DataFrame):
     
-    def new_cols(self,skip_first=None):
+    def new_cols(self, skip_first=None): #Used by other funcs
         
         null_cols = []
         for col in self.columns[1:] if skip_first else self.columns:
-            null_cols.append(col + "_new")
+            null_cols.append(col + "checked")
             col_pos = self.columns.get_loc(col)
             self.insert(col_pos + 1, null_cols[-1], None)
                     
@@ -18,7 +22,7 @@ class base(pd.DataFrame):
 
 
 
-    def implimentation(self,func,skip_first=None):
+    def implimentation(self, func,skip_first=None): #Used by other funcs
         
         null_cols = self.new_cols(skip_first)
         iter_nulls = iter(null_cols)
@@ -34,10 +38,7 @@ class base(pd.DataFrame):
             
         return self
             
-
-class CD(base):
     
-      
     def reduce_repeats(self): #Replaces words in place
 
         pattern = re.compile(r"(.)\1{2,}")
@@ -46,7 +47,6 @@ class CD(base):
             return pattern.sub(r"\1\1", str(word))
 
         return self.applymap(apply_reduction)
-
 
 
     def spell_check(self):
@@ -65,15 +65,58 @@ class CD(base):
             return process.extractOne(word, self.iloc[:,0])[0]
             
         return self.implimentation(apply_correction,skip_first=True)
+
+
+    
+    def embed(self, openai_api_key): #Used by other funcs
+        openai.api_key = openai_api_key
+        df_flat = self.values.flatten().tolist()
+        embeds = (openai.Embedding.create(model="text-embedding-ada-002",input=df_flat))
+        embeds = [ d['embedding'] for d in embeds['data']]
+        embeds = embeds/np.linalg.norm(embeds,axis=1,keepdims=True)
+        return embeds,df_flat
         
+    def k_means(self, openai_api_key, n_clusters=8):
+        embeds,df_flat = self.embed(openai_api_key)
+        cluster_model = KMeans(n_clusters=n_clusters,n_init="auto").fit(embeds)
+        array = cluster_model.labels_
+        
+        def formater(self,array):
+            pairs = {}
+            for num, val in zip(array, df_flat):
+                if num in pairs:
+                    pairs[num].append(val)
+                else:
+                    pairs[num] = [val]
+                    
+            long = 0
+            for num,vals in pairs.items():
+                if len(vals) > len(pairs[long]):
+                     long = num
+            for num in pairs:
+                if len(pairs[num]) < len(pairs[long]):
+                    pairs[num] += [np.nan] * (len(pairs[long])-len(pairs[num]))
+                    
+            return pd.DataFrame(pairs)
+        
+        return formater(self, array)
+    
+#For Testing
+def match(df): #Assumes first col contains valid values and last col contains checked values
+    if df.iloc[:, -1].isin(df.iloc[:, 0]).all():
+        return True
+    else:
+        lst = df[~df.iloc[:, -1].isin(df.iloc[:, 0])]
+        return lst
+        
+df = pd.read_csv(r"countries,cars,food.csv")
+openai_api_key = os.getenv("openai_api_key")
 
-countries = pd.read_csv("countries.csv")
-typos = pd.read_csv("typos.csv")
+pairs = CD(df).k_means(openai_api_key,n_clusters=3)
 
 
-removed = CD(typos).reduce_repeats()
 
-typos_checked = CD(typos).spell_check()
-countries_checked = CD(countries).spell_check()
-fuzzings = CD(countries).fuzzed()
+
+
+
 
