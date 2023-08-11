82d900917aa15c80eb6166b30f5c2d1f20cfcfe5
Mir Sajidur Rahman <msr.sajid1283@gmail.com>
2023-08-11 15:43:22 +0400

    ahnaf is gay

--- Fuzzy_Cleaning.py
+++ Fuzzy_Cleaning.py
@@ -1,53 +1,72 @@
+import os
 import pandas as pd
 import openai
-import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
-# Task 1: Meta Labelling
 
 data = pd.read_csv(r"https://raw.githubusercontent.com/MSajidur-Rahman/Fuzzy_Cleaning/main/Test_Data/Task1%20Data/Categories.csv")
-data = data.drop(columns = "Unnamed: 1").squeeze()
+data = data.drop(columns="Unnamed: 1").squeeze()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def Meta_Labelling(series,n):
-    
+def meta_labelling(series: pd.Series, n: int):
     """
-    Sorts a series into meta categories based on semantics.
+    Sorts a pandas series into meta categories based on semantics using GPT-3.5 model.
     
-    Args:
-    series (pd.Series): The input series to be sorted.
+    Parameters
+    ----------
+    series (pd.Series): The input series to be sorted. 
     n (int): The number of meta categories to create.
-    
-    Returns:
-    pd.DataFrame: A dataframe with the meta categories as columns and the sorted items as values.
+        
+    Returns
+    ------- 
+    (pd.DataFrame): A DataFrame with the meta categories as columns and the sorted items as values.
     """
     
-    system_message = f"""You will be provided a list and will sort them into {n} meta categories based on semantics.
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo",
+                 temperature=0.3)
+    
+    
+    template = ChatPromptTemplate.from_messages([
+    ("system", """You will be provided a list and will sort them into {number} meta categories based on semantics.
                     Return each category in one line. The format should be similar to the following:
                     Meta Label1: Item1, Item2, Item3
-                    Meta Label2: Item1, Item2, Item3"""
+                    Meta Label2: Item1, Item2, Item3"""),
                     
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    temperature = 0.4,
-    messages=[
-    {"role": "system", "content": f"{system_message}"},
-    {"role": "user", "content": f"{str(series.to_list())}"} 
-    ])
+    ("human", "{data}")])
+     
+    template.format_messages(number = str(n), data = str(series.to_list()))
     
-    response = completion.choices[0]["message"]["content"]
     
-    #converting the string response to a dataframe
+    response = llm(template)
+
+    return response_to_df(response)
+
+
+def response_to_df(response: str):
+    """
+    Convert a response string into a DataFrame of meta categories and sorted items.
+    
+    Parameters
+    ----------
+    response (str): The response string to be converted.
+        
+    Returns
+    ------- 
+    (pd.DataFrame): A DataFrame containing meta categories as columns and sorted items as values.
+    """
+    
     df_dict = {}
     for line in response.split("\n"):
         line = line.split(":")
         key = line[0]
         values = [val.strip() for val in line[1].split(",")]
         df_dict[key] = values
-        
+
     df = pd.DataFrame.from_dict(df_dict, orient='index').T
-    
     return df
 
-output = (Meta_Labelling(data, 8))
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+output = meta_labelling(data, 8)
